275940aeb6235ee95ad69f46442d75b86957760f
Alfredo Miranda <alfredocdmiranda@gmail.com>
2015-11-17 20:11:19 -0300

    new file:   examples/list_nodes.py 	new file:   examples/print_msg.py 	new file:   examples/thread.py message: "Added first examples"

---
+++ examples/thread.py
@@ -0,0 +1,25 @@
+import threading
+
+from pymys import mysensors as mys
+
+
+def run_gateway():
+    while True:
+        gw.process()
+
+
+def show_msg(msg):
+    print("Read: {}".format(msg))
+
+gw = mys.SerialGateway("/dev/ttyACM0", message_callback=show_msg)
+print("Trying to connect...")
+gw.connect()
+print("Connected!")
+
+t = threading.Thread(target=run_gateway)
+t.start()
+
+while True:
+    data = input("")
+    msg = mys.Message(data)
+    gw.send(msg)
